68fe976ca8d054e1ca35fbc30656aa819fbb87ad
Danny Cherry <dcherry@jackhenry.com>
2018-03-29 16:45:52 -0500

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] = [
+    {
+    "hostname" => "ec2-54-174-9-221.compute-1.amazonaws.com",
+    "ipaddress" => "54.174.9.221",
+    "port" => 80,
+    "ssl_port" => 80
+  }
+]
+
+include_recipe 'haproxy::manual'
